d6bf5f3f2066f70355864b24aae04c3ed897ff13
Hungjui Yu <yuhungjui@gmail.com>
2022-05-17 14:09:38 -0600

    minor update

--- ST_Correction/Intercomparison/Plot_scatter_ST_RS_1var.m
+++ ST_Correction/Intercomparison/Plot_scatter_ST_RS_1var.m
@@ -87,7 +87,7 @@ plot_var_ST = {[],[],[],[],[],[],[]};
 test_var_st = [];
 test_var_per = [];
 
-varid = 4;
+varid = 3;
 
 ssi = 1;
 
@@ -129,7 +129,7 @@ for vari = varid % one variable.
             ssi = ssi+1;
             
             %% FILTER DATA BY PRESSURE:
-            tmp_data_ST_pid = find(tmp_data_ST.P>=200);
+            tmp_data_ST_pid = find(tmp_data_ST.P>=0);
             tmp_data_ST.TIME_SEC = tmp_data_ST.TIME_SEC(tmp_data_ST_pid);
             eval([ 'tmp_data_ST.',process_vars_name{vari},' = tmp_data_ST.',process_vars_name{vari},'(tmp_data_ST_pid);' ]);
             
@@ -368,7 +368,7 @@ for vari = varid % one variable
     set(leg1,'LineWidth',ax_linewidth)
     
 end
-
+a
 % ==============================================================================
     
 %% Save Figure:
